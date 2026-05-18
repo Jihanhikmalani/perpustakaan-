0948538e5e391e0d30dfaffd80b4ae6c571c7e5e
jihanhikmalani <jhl503035@gmail.com>
2026-05-18 09:51:25 +0700

    update

--- main.js
+++ main.js
@@ -10,7 +10,8 @@ import {
   doc,
   getDoc,
   query,
-  where
+  where,
+  serverTimestamp
   
 } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-firestore.js";
 
@@ -24,20 +25,22 @@ const firebaseConfig = {
   appId: "1:996228614767:web:715bac465789728e9b9b9a"
 };
 
+
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app)
-const tanamanCollection = collection(db, "tanaman")
+const bukuCollection = collection(db, "buku")
+  // fungsi pinjam buku
+const peminjamanCollection = collection(db, "peminjaman")
+  
+  export async function login() {
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
 
-export async function login() {
-  const username = document.getElementById("username").value;
-  const password = document.getElementById("password").value;
-
-const q = query(
-  collection(db, "users"),
-  where("username", "==", username),
-  where("password", "==", password)
-)
-
+    const q = query(
+      collection(db,"users"),
+      where("username","==",username),
+      where("password","==", password)
+    )
   const querySnapshot = await getDocs(collection(db, "users"));
 
   let found = false;
@@ -55,10 +58,11 @@ const q = query(
     // redirect
     window.location.href = "admin.html";
   } else {
-    document.getElementById("status").innerText = "Username atau password salah"; 
+    document.getElementById("status").innerText = "Username atau password salah";
   }
-}
-//  fungsi untuk logout 
+  }
+  
+  //  fungsi untuk logout 
 export function logout() {
   // hapus status login dari localstorage
   localStorage.removeItem("isLogin")
@@ -66,5 +70,182 @@ export function logout() {
   // redirect ke halaman login
   window.location.href = "login.html"
 }
+
+  
+export async function tampilkanDaftarBuku() {
+  // ambil snapshot data dari koleksi tanaman
+  const snapshot = await getDocs(bukuCollection)
+  
+  // ambil element tabel data
+  const tabel = document.getElementById("tabelData")
+  
+  // ambil element tabel data
   
   
+  //kosongkan isi tablel 
+  tabel.innerHTML = ""
+  
+  //loop setiap dokumen dalam snapshot
+  snapshot.forEach((doc) => {
+    // variabel untuk menyimpan data 
+    const data = doc.data()
+    const id = doc.id
+    
+    // buat element baris baru
+    const baris = document.createElement("tr")
+    
+    const  no = document.createElement("td")
+    no.textContent = tabel.rows.length + 1
+    
+    //buat element kolom untuk nama tanaman 
+    const judulbuku = document.createElement("td")
+    judulbuku.textContent = data.judulbuku
+    
+    //buat element kolom untuk warna 
+    const penulis = document.createElement("td")
+    penulis.textContent = data.penulis
+    
+    // buat kolom jenis
+    const penerbit = document.createElement("td")
+    penerbit.textContent = data.penerbit
+    
+    // buat element kolom untuk Aksi
+    const aksi = document.createElement("td")
+    
+        // buat tombol edit
+    const tombolEdit = document.createElement("a")
+    tombolEdit.textContent = "Edit"
+    tombolEdit.href = "edit.html?id=" + id
+    tombolEdit.className = "button edit"
+    
+    //buat tombol hapus
+    const tombolHapus = document.createElement("button")
+    tombolHapus.textContent = "Hapus"
+    tombolHapus.className = "button delete"
+    tombolHapus.onclick = async () => {
+      await hapusBuku(id)
+    }
+    
+        //tambahkan element ke dalam kolom aksi
+    aksi.appendChild(tombolEdit)
+    aksi.appendChild(tombolHapus)
+    
+    //tambah kolom kedalam baris
+    baris.appendChild(no)
+    baris.appendChild(judulbuku)
+    baris.appendChild(penulis)
+    baris.appendChild(penerbit)
+    baris.appendChild(aksi)
+    
+    //tambahkan baris kedalam tabel
+    tabel.appendChild(baris)
+  })
+}
+
+export async function tambahDataBuku() {
+  //ambil nilai dari form
+  const judulbuku = document.getElementById('judulbuku').value
+  const penulis = document.getElementById('penulis').value
+  const penerbit = document.getElementById('penerbit').value
+  //tambahkan data ke firestore
+  await addDoc(bukuCollection, {
+    judulbuku: judulbuku,
+    penulis: penulis,
+    penerbit: penerbit
+  })
+  
+  //alihkan ke halaman daftar barang
+  window.location.href = 'admin.html'
+}
+
+export async function daftarBukuPublik() {
+  const snapshot = await getDocs(bukuCollection)
+  
+  const container = document.getElementById('container-buku')
+  
+  // ambil element tabel data
+  
+  
+  //kosongkan isi tablel 
+  container.innerHTML = ""
+  
+  
+  snapshot.forEach((doc)=>{
+        // variabel untuk menyimpan data 
+    const data = doc.data()
+    const id = doc.id
+    
+      //element kartu buku
+      const kartu = document.createElement("div")
+      kartu.className = "card"
+      
+      const judulbuku = document.createElement("h3")
+      judulbuku.textContent = data.judulbuku
+      
+      const penulis = document.createElement("p")
+      penulis.textContent = "penulis" + data.penulis
+      
+      
+      // buat element tombol pinjam
+      const tombolPinjam = document.createElement("button")
+      tombolPinjam.textContent ="Pinjam"
+      tombolPinjam.className ="button"
+      tombolPinjam.onclick = async () => {
+      tombolPinjam.disabled = true
+  await pinjamBuku(id, data)
+}
+      
+      
+      kartu.appendChild(judulbuku)
+      kartu.appendChild(penulis)
+      kartu.appendChild(tombolPinjam)
+      
+      
+      container.appendChild(kartu)
+  })
+}
+
+//fungsi untuk menghapus data barang
+export async function hapusBuku(id) {
+  if (!confirm("yakin ingin menghapus data ini?")) return
+  //menghapus dokumen barang berdasarkan id
+  await deleteDoc(doc(db, "buku", id))
+  
+  // refresh data Buku
+  await tampilkanDaftarBuku()
+}
+
+//fungsi untuk menampilkan data buku berdasarkan id
+export async function ambilDataBuku(id) {
+  const docRef = doc(db, "buku", id)
+  const docSnap = await getDoc(docRef)
+  
+  return await docSnap.data()
+}
+
+//fiungsi untuk mengubah data buku
+export async function ubahDataBuku(id, judulbuku, penulis, penerbit) {
+  await updateDoc(doc(db, "buku", id), {
+    judulbuku : judulbuku,
+    penulis : penulis,
+    penerbit : penerbit
+    
+  })
+  
+  //alihlkan ke halaman daftar buku
+  window.location.href = 'admin.html'
+  // Tab to edit
+}
+
+
+export async function pinjamBuku(id, dataBuku) {
+  await addDoc(peminjamanCollection, {
+    idBuku: id,
+    judulbuku: dataBuku.judulbuku,
+    penulis: dataBuku.penulis,
+    penerbit: dataBuku.penerbit,
+    tanggalPinjam: serverTimestamp()
+  })
+
+  alert("Buku berhasil dipinjam")
+}
